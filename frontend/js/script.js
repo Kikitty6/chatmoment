eaa2ce69d2521e3ffbafaadc9ae51cb909954a58
Kikitty6 <164757643+Kikitty6@users.noreply.github.com>
2024-07-30 03:00:06 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss:https://chatmoment-9zky.onrender.com")
     websocket.onmessage = processMessage
 }
 
